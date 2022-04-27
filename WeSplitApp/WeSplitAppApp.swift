db14b1c3819edc1729e76372b77144f62a2c08bf
Lucas Chae <hi@lucaschae.com>
2022-04-27 20:09:32 +0900

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Lucas Chae on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
